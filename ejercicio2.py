188960e1cea47663423dce379e7267a07a2f6a42
albertopalou <alberto.palouorellanapizarro@usp.ceu.es>
2022-10-27 19:52:43 +0200

    hecho

--- ejercicio2.py
+++ ejercicio2.py
@@ -17,3 +17,7 @@ class Alumno():
 alumno1 = Alumno("Manuel García", 8)
 alumno2 = Alumno("Jorge Paredes", 4)
 alumno3 = Alumno("Mario Fernandez", 5)
+
+print(alumno1.calificacion())
+print(alumno2.calificacion())
+print(alumno3.calificacion())
