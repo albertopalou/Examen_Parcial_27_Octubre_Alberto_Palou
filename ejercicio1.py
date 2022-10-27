7c3eadd0648bfb325f61c71e1b32491804807ee7
albertopalou <alberto.palouorellanapizarro@usp.ceu.es>
2022-10-27 19:33:32 +0200

    hecho

--- ejercicio1.py
+++ ejercicio1.py
@@ -5,9 +5,11 @@ class Alumno():
         self.nota = nota
         print(f"El alumno {self.nombre} se ha creado con exito")
 
-    def nota(self):
+    def calificacion(self):
         if self.nota >= 5:
             return(f"{self.nombre} ha aprobado con un {self.valor}.")
         else:
             return(f"{self.nombre} ha suspendido con un {self.valor}.")
-    
+
+alumno = Alumno("Manuel García", 8)
+print(alumno.calificacion)
